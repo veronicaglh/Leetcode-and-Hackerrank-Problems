fff8f0e343881e00779ac0c4306e1d11df13a77e
veronicaglh <veronicagt.IT@gmail.com>
2022-10-13 13:31:28 +0300

    add program to solve the less than sort problem on LeetCode

--- LeetCode/Sorting/LessThanSort.py
+++ LeetCode/Sorting/LessThanSort.py
@@ -11,41 +11,23 @@
 # This program will recieve a list of integers(named nums)
 # The program will display another list which will show how many numbers is less than each number in the original list 
 # For example if nums = [8,1,2,2,3]
-# The program then will output : [4,0,1,1,3]
+# Then result =  [4,0,1,1,3]
 # Meaning there are 4 numbers less than the number 8 in the initial list (nums)
 # There are no numbers less than the number one in the nums list 
 # There is 1 number less than the number 2 in the nums list etc..
+# The program will output the result list 
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
-        nums_zero = []
-        nums_one = []
-        nums_two = []
-        nums_three = []
-        nums_four = []
-        
+        result = []
+        count = 0 
         for i in range(len(nums)):
-            for j in range(1, len(nums)):
-                if nums[j] < nums[i]:
-                    if i == 0:
-                        nums_zero.append(nums[j])
-                
-                    elif i == 1:
-                        nums_one.append(nums[j])
-
-                    elif i == 2:
-                        nums_two.append(nums[j])
-
-                    elif i == 3:
-                        nums_three.append(nums[j])
-
-                    elif i == 4:
-                        nums_four.append(nums[j])
-
-
-        final_nums = [len(nums_zero), len(nums_one), len(nums_two), len(nums_three),len(nums_four)]
-        return final_nums
+            for j in range(len(nums)):
+                if nums[j]  < nums[i]:
+                    count += 1
+            result.append(count)
+            count = 0
+        return result 
     
         
         
