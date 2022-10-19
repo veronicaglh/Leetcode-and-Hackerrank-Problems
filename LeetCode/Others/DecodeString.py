c036301e2ad188878dc5531979a92b077034153d
veronicaglh <veronicagt.IT@gmail.com>
2022-10-19 22:24:04 +0300

    add program to solve the decode string problem on LeetCode

--- LeetCode/Others/DecodeString.py
+++ LeetCode/Others/DecodeString.py
@@ -1,3 +1,10 @@
+# LeetCode is a place where programmers from all over the world come together to solve different programming problems.
+# All of the questions done in this folder are from the LeetCode website. 
+# In order to access and solve the problems you will need to sign up for an account on their website. 
+# I have also attached a link to each problem solved so it is possible to access the problem and attempt to solve it.
+# Please run the code on LeetCodes own code editor. If you try to run it on your own IDE the code will not work.
+# Here is LeetCodes website: https://leetcode.com/
+# Here is where you can create an account: https://leetcode.com/accounts/signup/
 # This problem is called 394. Decode String
 # You can access it here: https://leetcode.com/problems/decode-string/
 
